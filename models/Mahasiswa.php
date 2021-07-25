2b22578d2afb79478ca75d24ee9beff9941aae67
Ulil Ambri <80489540+UlilAmbri@users.noreply.github.com>
2021-07-26 00:55:04 +0700

    Add files via upload

--- models/Mahasiswa.php
+++ models/Mahasiswa.php
@@ -33,15 +33,18 @@ class Mahasiswa extends \yii\db\ActiveRecord
     public function rules()
     {
         return [
-            [['nim', 'nama', 'jekel', 'tgl', 'id_fakultas', 'id_prodi', 'email', 'alamat'], 'required'],
+            [['nim', 'nama', 'jekel', 'tgl', 'id_fakultas', 'id_prodi', 'id_provinsi', 'id_kab', 'email', 'alamat', 'foto'], 'required'],
             [['tgl'], 'safe'],
             [['id_fakultas', 'id_prodi'], 'integer'],
             [['nim'], 'string', 'max' => 18],
             [['nama', 'email'], 'string', 'max' => 50],
             [['jekel'], 'string', 'max' => 1],
-            [['alamat'], 'string', 'max' => 100],
+            [['alamat', 'foto'], 'string', 'max' => 100],
             [['id_fakultas'], 'exist', 'skipOnError' => true, 'targetClass' => Fakultas::className(), 'targetAttribute' => ['id_fakultas' => 'id_fakultas']],
             [['id_prodi'], 'exist', 'skipOnError' => true, 'targetClass' => Prodi::className(), 'targetAttribute' => ['id_prodi' => 'id']],
+            [['id_provinsi'], 'exist', 'skipOnError' => true, 'targetClass' => Provinces::className(), 'targetAttribute' => ['id_provinsi' => 'id']],
+            [['id_kab'], 'exist', 'skipOnError' => true, 'targetClass' => Regencies::className(), 'targetAttribute' => ['id_kab' => 'id']],
+            // [['foto'], 'file', 'skipOnEmpty' => false, 'extensions' => 'png, jpg, jpeg, jfif'],
         ];
     }
 
@@ -58,9 +61,24 @@ class Mahasiswa extends \yii\db\ActiveRecord
             'tgl' => 'Tgl',
             'id_fakultas' => 'Id Fakultas',
             'id_prodi' => 'Id Prodi',
+            'id_provinsi' => "Id Provinsi",
+            'id_kab' => "Id Kab/Kota",
             'email' => 'Email',
             'alamat' => 'Alamat',
+            'foto' => 'Foto',
         ];
+    }
+
+    public function upload()
+    {
+        if ($this->validate()) {
+            // $this->imageFile->saveAs('uploads/' . $this->imageFile->baseName . '.' . $this->imageFile->extension);
+            $this->imageFile->saveAs(file_get_contents($this->imageFile->tempName));
+            // $this->imageFile->saveAs("ulililil");
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public function getFakultas()
@@ -72,4 +90,20 @@ class Mahasiswa extends \yii\db\ActiveRecord
     {
         return $this->hasOne(Prodi::className(), ['id' => 'id_prodi']);
     }
+
+    public function getRegencies()
+    {
+        return $this->hasOne(Regencies::className(), ['id' => 'id_kab']);
+    }
+
+    public function getProvinces()
+    {
+        return $this->hasOne(Provinces::className(), ['id' => 'id_provinsi']);
+    }
+
+    //  public function getProvince()
+    // {
+    //     return $this->hasOne(Provinces::className(), ['id' => 'id_provinsi']);
+    // }
+
 }
